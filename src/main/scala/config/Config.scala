8ab21f91eca6bf69479c8de82a59e6a1a9fc3790
alduk <arsen.kudla@gmail.com>
2014-02-10 14:47:11 +0200

    asd

--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -4,5 +4,5 @@ import com.typesafe.slick.driver.db2.DB2Driver.simple._
 
 object Config {
   val driver = com.typesafe.slick.driver.db2.DB2Driver.simple
-  val db = Database.forURL("jdbc:db2://wsc-dev-db-01.kiev.ecofabric.com:50001/HMTKDB2", driver = "COM.ibm.db2.jdbc.app.DB2Driver", user = "db2inst1", password = "passw0rd")
+  
 }
