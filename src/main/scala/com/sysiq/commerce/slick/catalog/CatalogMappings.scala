efbb532ae1e0a7f62f5d91f8860b7fe11dfe0b3b
alduk <arsen.kudla@gmail.com>
2014-02-11 17:18:44 +0200

    Catalog groups added

--- src/main/scala/com/sysiq/commerce/slick/catalog/CatalogMappings.scala
+++ src/main/scala/com/sysiq/commerce/slick/catalog/CatalogMappings.scala
@@ -13,4 +13,25 @@ object CatalogMappings {
   }
 
   val Catalogs = TableQuery[Catalogs]
+
+  case class CatalogGroup(id: Long, memberId: Long, identifier: String, markForDelete: Int)
+  class CatalogGroups(tag: Tag) extends Table[CatalogGroup](tag, "CATGROUP") {
+    def id = column[Long]("CATGROUP_ID", O.PrimaryKey)
+    def memberId = column[Long]("MEMBER_ID")
+    def identifier = column[String]("IDENTIFIER")
+    def markForDelete = column[Int]("MARKFORDELETE")
+    def * = (id, memberId, identifier, markForDelete) <> (CatalogGroup.tupled, CatalogGroup.unapply)
+  }
+
+  val CatalogGroups = TableQuery[CatalogGroups]
+
+  case class CatalogToGroupRelation(catalogId: Long, groupId: Long)
+  class CatalogToGroupRelations(tag: Tag) extends Table[CatalogToGroupRelation](tag, "CATGROUP") {
+    def catalogId = column[Long]("CATALOG_ID")
+    def groupId = column[Long]("CATGROUP_ID")
+    def * = (catalogId, groupId) <> (CatalogToGroupRelation.tupled, CatalogToGroupRelation.unapply)
+    def pk = primaryKey("<SYSTEM-GENERATED>", (catalogId, groupId))
+  }
+
+  val CatalogToGroupRelations = TableQuery[CatalogToGroupRelations]
 }
